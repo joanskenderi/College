620898fb14d9700d63c440207eb496ad682bda6d
Joan Skënderi <joanskenderi@192.168.1.4>
2022-06-16 04:14:52 +0200

    C Commit🔥

--- C/Biggest Number/main.c
+++ C/Biggest Number/main.c
@@ -5,17 +5,17 @@
 int main() {
     int firstNumber, secondNumber, thirdNumber;
 
-    printf("Vendos tre numra: \n");
+    printf("Enter 3 numbers: ");
     scanf("%d %d %d", &firstNumber, &secondNumber, &thirdNumber);
 
-    if(firstNumber >= secondNumber && firstNumber >= thirdNumber)
-        printf("%d eshte numri me i madh \n", firstNumber);
+    if (firstNumber >= secondNumber && firstNumber >= thirdNumber)
+        printf("%d is the biggest number.", firstNumber);
 
-    if(secondNumber >= firstNumber && secondNumber >= thirdNumber)
-        printf("%d eshte numri me i madh \n", secondNumber);
+    if (secondNumber >= firstNumber && secondNumber >= thirdNumber)
+        printf("%d is the biggest number.", secondNumber);
 
-    if(thirdNumber >= firstNumber && thirdNumber >= secondNumber)
-        printf("%d eshte numri me i madh \n", thirdNumber);
+    if (thirdNumber >= firstNumber && thirdNumber >= secondNumber)
+        printf("%d is the biggest number.", thirdNumber);
 
     return 0;
 }
